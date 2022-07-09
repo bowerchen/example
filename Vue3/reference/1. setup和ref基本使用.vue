6247edfdc12004d473f1add56480fae00b5a56cb
bowerchen <bowerfullstack@gmail.com>
2022-07-09 22:57:18 +0800

    Vue3 学习

---
+++ Vue3/reference/1. setup和ref基本使用.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>Vue3 学习</div>
+  <h1>{{name}}</h1>
+
+  <h2>setup和ref的基本使用</h2>
+  <div>{{count}}</div>
+  <button @click="updateCount">更新数据</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    let name = 10
+    let count = ref(0)
+    function updateCount() {
+      count.value++
+      console.log(count.value, "=======")
+    }
+    return {
+      name,
+      count,
+      updateCount
+    }
+  },
+  methods: {
+
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue3/reference/2. reactive使用.vue
@@ -0,0 +1,40 @@
+<template>
+  <h2>reactive的使用</h2>
+  <h3>name: {{user.name}}</h3>
+  <h3>name: {{user.age}}</h3>
+  <h3>name: {{user.wife}}</h3>
+  <Button @click="update">更新数据</Button>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    let user = reactive({
+      name: 'bowerchen',
+      age: 18,
+      wife: {
+        name: 'marry',
+        age: 22
+      }
+    })
+    console.log(user.name,user.wife.name)
+    const update=() => {
+      user.name += '--'
+      user.age += 5
+      user.wife.name += '++'
+      user.wife.age +=5
+    }
+
+    return {
+      user,
+      update
+    }
+  }
+});
+</script>
+
+<style>
+</style>
