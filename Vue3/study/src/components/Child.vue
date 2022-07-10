4cb43ab88fad33ec7a4920492c8d45fc13cd4bf1
bowerchen <bowerfullstack@gmail.com>
2022-07-10 18:11:51 +0800

    setup基本使用

---
+++ Vue3/study/src/components/Child.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2>Child子组件</h2>
+  <h3>msg: {{msg}}</h3>
+
+  <button @click="update">更新</button>
+</template>
+
+<script lang="ts">
+import { defineComponent ,ref } from 'Vue'
+export default defineComponent({
+  name: "Child",
+  props: ["msg"],
+  setup(props, {attrs, emit, slots}) {
+    console.log(props.msg, attrs.msg2, slots, emit)
+
+    const m = ref(3)
+    const n = ref(2)
+
+    function update() {
+      emit('xxx', '++')
+    }
+    return {
+      update,
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
